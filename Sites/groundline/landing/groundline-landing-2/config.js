e5566260364747648d0656664e8182560662b416
Aizer Ravelo <aizer@groundline.ph>
2021-10-18 13:41:52 +0800

    wip: create cusotom  @builder

---
+++ Sites/groundline/landing/groundline-landing-2/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteTitle: 'Groundline Software Solutions', // <title>
+  manifestName: 'Groundline PH',
+  manifestShortName: 'GroundlinePH', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#000000',
+  manifestThemeColor: '#000000',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/images/logo.png',
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'groundline',
+  subHeading: 'We thrive when coming up with innovative ideas.',
+
+  // social
+  socialLinks: [
+    {
+      icon: 'icon-social-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'icon-social-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'icon-social-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+  ],
+  email: 'hello@groundline.ph',
+  phone: '000-00000',
+  address: '',
+};
